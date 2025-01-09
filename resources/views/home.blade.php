a88fa21592dea51f094131e66d492fce8e8b0053
DeDinnich <lucas.dinnichert.pro#gmail.com>
2025-01-09 15:40:07 +0100

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layout.app')
 
 @section('head-css')
     <link href="css/home.css" rel="stylesheet">
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,6 +10,8 @@
     <!-- Core CSS Files -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/fontAwesome/css/all.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
+    <link href="css/layout.css" rel="stylesheet">
 
     <!-- Additional CSS pushed by views -->
     @yield('head-css')
